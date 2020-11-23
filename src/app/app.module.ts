e2802ced9d9ec3246b6d13013e51a12e9c59963c
Daniel Jaworski <daniel.jaworski@merce.com>
2020-11-23 15:03:21 +0100

    NGRX State

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { StoreModule } from '@ngrx/store';
 
 import { AppRoutingModule } from './app-routing.module';
+import { counterReducer } from './posts/store/counter.reducer';
 
 import { AppComponent } from './app.component';
 import { Error404Component } from './error404/error404.component';
 import { MainPageComponent } from './main-page/main-page.component';
+import { environment } from 'src/environments/environment';
 // import { TodosModule } from './todos/todos.module';
 // import { PostsModule } from './posts/posts.module';
 
@@ -19,7 +23,12 @@ import { MainPageComponent } from './main-page/main-page.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule,
+    HttpClientModule, 
+    StoreModule.forRoot({count: counterReducer}),
+    StoreDevtoolsModule.instrument({
+      maxAge: 25, 
+      logOnly: environment.production,
+    }),
     // TodosModule,
     // PostsModule
   ],
--- src/app/posts/posts.component.ts
+++ src/app/posts/posts.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Observable } from 'rxjs';
+import { Store } from '@ngrx/store';
+import { increment, decrement, reset } from './store/post.actions';
 import { map } from 'rxjs/operators'
 
 import { Post } from './shared/post.model';
@@ -12,10 +14,23 @@ import { Post } from './shared/post.model';
 })
 export class PostsComponent  {  
   posts$: Observable<Post[]>;
+  count$: Observable<number>
 
-  constructor(private activatedRoute: ActivatedRoute) {
+  constructor(private activatedRoute: ActivatedRoute, private store: Store<{count: number}>) {
+    this.count$ = store.select('count');
     this.posts$ = this.activatedRoute.data.pipe(
       map((data: { posts: Post[]}) => data.posts)
     );
+  }
+  increment() {
+    this.store.dispatch(increment());
+  }
+ 
+  decrement() {
+    this.store.dispatch(decrement());
+  }
+ 
+  reset() {
+    this.store.dispatch(reset());
   }
 }
